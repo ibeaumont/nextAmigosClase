b9ffcffa51469c6b533f33329a73c91448314fe1
ibeaumont <ibeaumont@zubirimanteo.com>
2015-11-18 22:16:39 +0100

    froga

---
+++ js/controllers.js
@@ -0,0 +1,29 @@
+var amigosControllers = angular.module('amigosControllers', []);
+
+amigosControllers.controller('amigosCtrl', ['$scope','$rootScope',
+  function($scope,$rootScope) {
+    $rootScope.amigos = [
+	{
+		nombre:"juan",
+		tlfno:"123456789"
+	},
+	{
+		nombre:"pedro",
+		tlfno:"123456789"
+	},	
+	{
+		nombre:"luis",
+		tlfno:"123456789"
+	}
+	];
+	
+  }]);
+amigosControllers.controller('amigoEditCtrl', ['$scope', '$rootScope','$routeParams',
+  function($scope,$rootScope,$routeParams) {
+  	/*for (i=0;i<$rootScope.amigos.length;i++){
+  		if ($rootScope.amigos[i].nombre==$routeParams.amigoId){
+  		 $scope.amigo=$rootScope.amigos[i];
+  		}
+	}*/
+	$scope.amigo=$rootScope.amigos[$routeParams.amigoId];
+  }]);
